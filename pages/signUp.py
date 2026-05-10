0094c0b93e9b99d5eff34a7f32f87b1ca2c9fab8
khishame <neo.khishame@gmail.com>
2026-05-10 13:11:53 +0200

    Fix page switch path after user account creation

--- pages/signUp.py
+++ pages/signUp.py
@@ -216,7 +216,7 @@ if st.button("Create Account"):
 
         if success:
             st.success("Account created successfully!")
-            st.switch_page("pages/EasyJobsWebApp.py")
+            st.switch_page("EasyJobsWebApp.py")
         else:
             st.error(msg)
 
