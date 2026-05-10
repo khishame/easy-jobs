fc77ef7d99d670b07c7b7b924315ac44e675dc66
khishame <neo.khishame@gmail.com>
2026-05-10 15:01:48 +0200

    Update notifications.py

--- pages/notifications.py
+++ pages/notifications.py
@@ -58,7 +58,7 @@ def delete_user_account(user_id):
 
 st.set_page_config(page_title="Notifications - Easy Jobs", page_icon="🔔", layout="centered")
 
-st.markdown("<style>[data-testid='stSidebar'] {display: none;}</style>", unsafe_allow_html=True)
+st.markdown("<style>[data-testid='stSidebarNav'] {display: none;}</style>", unsafe_allow_html=True)
 
 st.markdown("""
 <style>
